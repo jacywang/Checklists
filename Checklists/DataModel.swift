99f4d7388c4f2f64763e17bd6670eefb8abd5fb0
Jacy Wang <jacywj@gmail.com>
2015-03-22 20:24:39 -0400

    Add sorting functionality

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -35,6 +35,7 @@ class DataModel {
                 let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
                 lists = unarchiver.decodeObjectForKey("Checklists") as [Checklist]
                 unarchiver.finishDecoding()
+                sortChecklists()
             }
         }
     }
@@ -71,5 +72,12 @@ class DataModel {
             indexOfSelectedChecklist = 0
             userDefaults.setBool(false, forKey: "FirstTime")
         }
+    }
+    
+    
+    func sortChecklists() {
+        lists.sort({checklist1, checklist2 in return
+            checklist1.name.localizedStandardCompare(checklist2.name) ==
+                NSComparisonResult.OrderedAscending })
     }
 }
